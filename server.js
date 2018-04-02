41b0fb434306b96392961bd4663198f650af0687
Scott Saltzman <salty923@gmail.com>
2018-04-01 21:20:02 -0500

    heroku error

--- server.js
+++ server.js
@@ -26,37 +26,30 @@ app.use(routes);
 // var databaseUri = "mongodb://localhost/meetups_db";
 
 //connect to db
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScrape_db";
-
-// if (process.env.MONGODB_URI) {
-//     mongoose.connect(process.env.MONGODB_URI);
-// }else{
-//     mongoose.connect(databaseUri);
-// }
-
-var db = mongoose.connection;
-
-db.on('error',function (err) {
-    console.log('Mongoose Error: ',err);
-});
-
-db.once('open', function(){
-    console.log('Mongoose connection successful.');
-});
-
-// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/meetups_db";
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-    // useMongoClient: true
+   // useMongoClient: true
 });
 
+var db = mongoose.connection;
+
+// db.on('error',function (err) {
+//     console.log('Mongoose Error: ',err);
+// });
+
+// db.once('open', function(){
+//     console.log('Mongoose connection successful.');
+// });
 
 
-//maybe try that 
+
+
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
