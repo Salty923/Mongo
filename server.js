edc89516febabeb14cc2adcabd87d9a9c055091d
Scott Saltzman <salty923@gmail.com>
2018-04-01 20:50:30 -0500

    heroku error

--- server.js
+++ server.js
@@ -23,17 +23,33 @@ app.use(routes);
 
 
 
+var databaseUri = "mongodb://localhost/meetups_db";
 
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+}else{
+    mongoose.connect(databaseUri);
+}
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/meetups_db";
+var db = mongoose.connection;
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-    // useMongoClient: true
+db.on('error',function (err) {
+    console.log('Mongoose Error: ',err);
 });
+
+db.once('open', function(){
+    console.log('Mongoose connection successful.');
+});
+
+// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/meetups_db";
+
+// // Set mongoose to leverage built in JavaScript ES6 Promises
+// // Connect to the Mongo DB
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI, {
+//     // useMongoClient: true
+// });
 
 
 
